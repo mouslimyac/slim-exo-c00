7fe341e33dd029bb47577ed6ff449ffe6a09ca72
mouslimyac <mouslimyac@gmail.com>
2025-05-14 15:01:20 +0100

    modification

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -3,7 +3,7 @@
 #read -p "Entrez le nom du fichier : " filename
 echo "Entrez le nom du fichier :"
 read filename
-if [ -f $filename ]; then
+if [ -f '$filename' ]; then
     echo "Le fichier '$filename' existe."
 else
     echo "Le fichier '$filename' n'existe pas."
